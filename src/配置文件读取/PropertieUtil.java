de80c30766720ed231fe31126901282ad41a0b68
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2018-08-17 09:13:33 +0800

    修改配置文件读取代码

--- src/配置文件读取/PropertieUtil.java
+++ src/配置文件读取/PropertieUtil.java
@@ -8,17 +8,37 @@ import java.util.Properties;
  * Created by Yingjie.Lu on 2018/7/26.
  */
 public class PropertieUtil {
+
+    public static Properties getProer(String path){
+        Properties pro=null;
+        //从文件中读取
+        FileReader in= null;//获取输入流
+        try {
+            in = new FileReader(path);
+            pro=new Properties();
+            pro.load(in);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(in!=null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return pro;
+    }
+
+
     public static void main(String[] args) throws Exception {
 
-        //从文件中读取
-        FileReader in=new FileReader("src\\配置文件读取\\pro.txt");//获取输入流
-
-        Properties pro=new Properties();
-        pro.load(in);
+        Properties pro=PropertieUtil.getProer("src\\配置文件读取\\pro.txt");
 
         String value=pro.getProperty("name");
         System.out.println(value);
-
-        in.close();
     }
 }
